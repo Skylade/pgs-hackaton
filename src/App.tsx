3a7ccda9ea070f3e5f81631470747d21becfb643
Jakub Kalina <kalinakuba6@gmail.com>
2021-11-19 21:03:26 +0100

    Added working map

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Col, Row } from 'antd';
 import 'antd/dist/antd.css';
 import './App.scss'
-// import OpenStreetMap from './OpenStreetMap/OpenStreetMap';
+import OpenStreetMap from './OpenStreetMap/OpenStreetMap';
 
 
 function App() {
@@ -11,17 +11,18 @@ function App() {
   return (
     <div className='landing-container'>
 
+
       <Row className='landing-container__second-row'>
         <Col xs={24} sm={24} md={12} lg={12} xl={12} className='landing-container__second-row__first-col'>
           Content
         </Col>
         <Col xs={24} sm={24} md={12} lg={12} xl={12} className='landing-container__second-row__second-col'>
-          {/* <OpenStreetMap
+          <OpenStreetMap
             center={[51.06370, 17.03760]}
             markerPosition={[51.06370, 17.03760]}
-            address={'pawia 44 wrocław'}
-            zoom={30}
-          /> */}
+            address={'wrocław'}
+            zoom={12}
+          />
         </Col>
       </Row>
 
--- src/OpenStreetMap/OpenStreetMap.tsx
+++ src/OpenStreetMap/OpenStreetMap.tsx
@@ -1,57 +1,50 @@
-import { tmpdir } from "os";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { Icon, LatLngExpression } from "leaflet";
+import React from 'react'
 
-export default tmpdir;
-// import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
-// import { Icon, LatLngExpression } from "leaflet";
-// import React from 'react'
+import './OpenStreetMap.scss'
 
-// import './OpenStreetMap.less'
+export const icon = new Icon({
+    iconUrl: require('./utils/home.svg'),
+    iconSize: [50, 50]
+});
 
-// export const icon = new Icon({
-//     iconUrl: require('./utils/home.svg'),
-//     iconSize: [50, 50]
-// });
+interface OpenStreetMapProps {
+    markerPosition: LatLngExpression
+    center: LatLngExpression
+    address: string
+    zoom: number
+}
 
-// interface OpenStreetMapProps {
-//     markerPosition: LatLngExpression
-//     center: LatLngExpression
-//     address: string
-//     zoom: number
-// }
-
-// function ChangeView({ center, zoom }) {
+// function ChangeView( center: any, zoom: any ) {
 //     const map = useMap();
 //     map.setView(center, zoom);
 //     return null;
 // }
 
 
-// const OpenStreetMap = (props: OpenStreetMapProps) => {
-//     const { markerPosition, address, zoom, center } = props
+const OpenStreetMap = (props: OpenStreetMapProps) => {
+    const { markerPosition, address, zoom, center } = props
 
 
-//     return (
-//         <MapContainer center={center} zoom={zoom}>
-//             <TileLayer
-//                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-//             />
-//             <ChangeView center={center} zoom={zoom} />
-//             <Marker
-//                 key={1}
-//                 position={markerPosition}
-//                 riseOnHover
-//                 icon={icon}
-//             >
-//                 <Popup position={markerPosition}>
-//                     <div>
-//                         <h3>Znaleziony adres </h3>
-//                         <p>{address}</p>
-//                     </div>
-//                 </Popup>
-//             </Marker>
-//         </MapContainer>
-//     );
-// }
+    return (
+        <MapContainer    style={{ height: '100vh', width: '100wh' }}
+        center={center} zoom={zoom}>
 
-// export default OpenStreetMap
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+            {/* <ChangeView center={center} zoom={zoom} /> */}
+            {/* <Marker
+                key={1}
+                position={markerPosition}
+                riseOnHover
+                icon={icon}
+            >
+            </Marker> */}
+        </MapContainer>
+    );
+}
+
+export default OpenStreetMap
