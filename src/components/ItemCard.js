3241a11e89df3b6db450097f0bdb982aee8df169
Patryk <patryk.ganczarek@gmail.com>
2021-11-20 00:01:03 +0100

    Cards, images

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,10 +1,11 @@
 import React from "react";
+import imageSerek from "../images/serek.jpg";
 
 function ItemCard(props) {
     return (
         <div className="card-container">
             <div className="card-image">
-                <img src={props.item.image} alt="" />
+                <img src={props.item.imageUrl} alt="image" />
             </div>
             <div className="card-details">
                 <div className="card-title"> {props.item.name} </div>{" "}
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,7 +1,17 @@
 import React from "react";
 import { List, Typography, Divider } from "antd";
 import ItemCard from "./ItemCard";
-//import serekImage from "../images/serek";
+//import imagePifko from "../images/pifko.jpg";
+import imageSerek from "../images/serek.jpg";
+import imageChleb from "../images/chleb.png";
+import imageCynamonka from "../images/cynamonka.jpg";
+import imageJajka from "../images/jajka.jpg";
+import imageKabanosy from "../images/kabanosy.png";
+import imageKielbasa from "../images/kielbasa.jpg";
+import imageBorowiki from "../images/borowiki.jpg";
+import imageSokKapusta from "../images/sok-kapusty.jpg";
+import imageYerba from "../images/yerba.jpg";
+//import data from "./dataStore.js";
 
 function ItemList() {
     const data = [
@@ -11,7 +21,15 @@ function ItemList() {
             price: "16",
             location: "Wrocław",
             owner: "Dobre serki",
-            image: "",
+            imageUrl: imageSerek,
+            label: "",
+        },
+        {
+            name: "Piwo craftowe",
+            price: "16",
+            location: "Wrocław",
+            owner: "Dobre pifko",
+            image: imageCynamonka,
             label: "",
         },
     ];
